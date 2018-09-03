f45bc1f601ba3d0bd7d51dc7dee640e2eec98779
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 16:16:04 +0800

    add filter choose common elements

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function grouping_count(collection) {
-
   //在这里写入代码
+  // let countMap = new Map();
+  // collection.forEach(i => {
+  //   const baseCount = countMap.has(i) ? countMap.get(i) : 0;
+  //   countMap.set(i, baseCount + 1);
+  // });
+  // console.log(countMap);
+  // return countMap;
 }
 
 module.exports = grouping_count;
--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  return collection_a.filter(i => collection_b.includes(i));
 }
 
 module.exports = choose_common_elements;
