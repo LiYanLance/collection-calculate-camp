865090afe310e6587ee0d03afbb11a55921a0b56
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 21:46:20 +0800

    interval operation average uneven

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,9 @@
 'use strict';
 
-function average_uneven(collection) {
+const getAverage = arr => arr.reduce((a, b) => a + b) / arr.length;
 
-  //在这里写入代码
+function average_uneven(collection) {
+  return getAverage(collection.filter(i => i % 2 !== 0));
 }
 
 module.exports = average_uneven;
