bc794c2f8d2bf2482734a0bdaa8814b0e4426780
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 20:09:42 +0800

    count same elements

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,12 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = [];
+  let countMap = new Map();
+  collection.forEach(num => {
+    const default_count = (countMap.has(num)) ? countMap.get(num) : 0;
+    countMap.set(num, default_count + 1);
+  })
+  countMap.forEach((val, key) => {result.push({'key': key, 'count': val})});
+  return result;
 }
 
 module.exports = count_same_elements;
