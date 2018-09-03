a275dbd82c72a059ad148b404c259fdb57311839
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 00:25:35 +0800

    filter grouping count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,14 +1,15 @@
 'use strict';
 
 function grouping_count(collection) {
-  //在这里写入代码
-  // let countMap = new Map();
-  // collection.forEach(i => {
-  //   const baseCount = countMap.has(i) ? countMap.get(i) : 0;
-  //   countMap.set(i, baseCount + 1);
-  // });
-  // console.log(countMap);
-  // return countMap;
+  return collection.reduce((letters, letter) => {
+    if (letter in letters) {
+      letters[letter]++;
+    }
+    else {
+      letters[letter] = 1;
+    }
+    return letters;
+  }, {});
 }
 
 module.exports = grouping_count;
