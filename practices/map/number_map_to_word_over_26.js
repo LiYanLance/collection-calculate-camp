be68a09be2d3bd594ad436b4514bd2fd5e02df23
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 17:47:04 +0800

    add map number to word over 26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,9 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(i => {
+    let resultNum = (i > 26 ? String.fromCharCode(i / 26 + 96) : "");
+    return resultNum + String.fromCharCode(i % 26 + 96);
+  });
 };
 
 module.exports = number_map_to_word_over_26;
