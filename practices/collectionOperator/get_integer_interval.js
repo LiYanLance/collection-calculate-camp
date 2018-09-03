8ea88a93bfc012d5fb84b33532b4f8195cecd75b
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 22:57:41 +0800

    collection operator get integer interval

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  let result= [...Array(Math.abs(number_a - number_b) + 1).keys()].map(x => x + Math.min(number_b, number_a));
+  return number_a < number_b ? result : result.reverse();
 }
 
 module.exports = get_integer_interval;
