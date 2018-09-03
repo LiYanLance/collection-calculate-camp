ac44890f5d260433a35b6adc28bf1f1114031498
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 21:08:04 +0800

    update collection element.count divide by 3

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,11 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let collection = collection_a.concat();
+  return collection_a.map(val => {
+    return {
+      'key': val.key,
+      "count" :object_b.value.includes(val.key) ? val.count - parseInt(val.count / 3) : val.count
+    }
+  });
 }
 
 module.exports = create_updated_collection;
