05bf62dea544c6b8a26b0dc5c85e1d0eebee560a
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 15:45:54 +0800

    add union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,13 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  let collection_c = collection_a.concat();
+  collection_b.forEach(item => {
+    if (!collection_c.includes(item)) {
+      collection_c.push(item);
+    }
+  });
+  return collection_c;
 }
 
 module.exports = get_union;
