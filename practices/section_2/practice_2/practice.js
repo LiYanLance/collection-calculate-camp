e66c29bb8cf33a7f2497d5359a2fa2a18a769d0d
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 20:25:26 +0800

    count same elements includes dash

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,17 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = [];
+  let countMap = new Map();
+  collection.forEach(num => {
+    let sub_count = 1;
+    if(num.includes('-')){
+      sub_count = parseInt(num.substring(num.indexOf('-') + 1));
+      num = num.substring(0, num.indexOf('-'));
+    }
+    const default_count = (countMap.has(num)) ? countMap.get(num) : 0;
+    countMap.set(num, default_count + sub_count);
+  })
+  countMap.forEach((val, key) => {result.push({'key': key, 'count': val})});
+  return result;
 }
 
 module.exports = count_same_elements;
