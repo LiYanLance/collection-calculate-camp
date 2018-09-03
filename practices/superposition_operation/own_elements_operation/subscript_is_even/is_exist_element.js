740b5a4c278d47e73c2c47daa0a961ead11aff33
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 22:11:43 +0800

    subscript is even - is exist element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,10 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  for(let index = 0; index < collection.length; index++){
+    if(collection[index] === element && index % 2 === 0){
+      return true;
+    }
+  }
+  return false;
 };
 module.exports = is_exist_element;
