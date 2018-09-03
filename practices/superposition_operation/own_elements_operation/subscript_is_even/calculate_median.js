f620a896badb340c5d2126a6f849ccf1e84c4320
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 22:08:47 +0800

    subscript is even calculate median

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,8 @@
 'use strict';
-var calculate_median = function(collection){
 
+const getAverage = arr => arr.reduce((a, b) => a + b) / arr.length;
+
+var calculate_median = function(collection){
+  return getAverage(collection.filter(i => i % 2 === 0));
 };
 module.exports = calculate_median;
