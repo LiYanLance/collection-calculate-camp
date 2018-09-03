0014a9f218d25863963c46afe83dae1e59ffdf21
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 16:42:28 +0800

    add filter choose no common elements

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,8 +3,8 @@
 function choose_no_common_elements(collection_a, collection_b) {
   //在这里写入代码
   // a [1,2,3,4,5]  b[2,3,4,5,6]  => [1,6]
-  let collection_c = collection_a.filter(a => !collection_b.includes(a));
-  return collection_c.concat(collection_b.filter(b => !collection_a.includes(b)));
+  return Array.from(new Set(collection_a)).concat(Array.from(new Set(collection_b)))
+    .filter((num,_,arry) => arry.indexOf(num) === arry.lastIndexOf(num));
 }
 
 module.exports = choose_no_common_elements;
