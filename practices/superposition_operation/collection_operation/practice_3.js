4fd4265f109e2b6a51be2b51965657da7d922793
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 21:38:05 +0800

    collection operation get sum of odd times 3 add 5

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  return collection.filter(i => i % 2 !== 0)
+    .map(odd => odd * 3 + 5)
+    .reduce((a, b) => a + b);
 }
 
 module.exports = hybrid_operation_to_uneven;
