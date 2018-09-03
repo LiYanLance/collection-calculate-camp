fc96d3ec1dbc7d0ba8a3f30293c4756ac1d8b176
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 21:33:13 +0800

    collection operation average to letter

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function average_to_letter(collection) {
-
-  //在这里写入代码
+  return String.fromCharCode(Math.round(collection.reduce((a, b) => a + b) / collection.length) + 96);
 }
 
 module.exports = average_to_letter;
