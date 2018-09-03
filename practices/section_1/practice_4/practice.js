3666998c5c2c07fc915dab9ad899ec88d8e3156f
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 19:54:01 +0800

    collect same elements in keys and object

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,7 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  let collection = [];
+  collection_a.map(a => collection.push(a.key));
+  return collection.filter(num => object_b.value.includes(num));
 }
 
 module.exports = collect_same_elements;
