bde944f85cf1d3235f95e1edeaea9180c333ec3c
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 21:57:30 +0800

    interval operation median to letter

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,12 @@
 'use strict';
 
-function median_to_letter(collection) {
+const get_word_by_letter = (number) => String.fromCharCode(number + 96);
 
-  //在这里写入代码
+function median_to_letter(collection) {
+  let median = collection[collection.length / 2];
+  median = collection.length % 2 === 0 ? parseInt((median + collection[collection.length / 2 + 1]) / 2) : median;
+  const front = median > 26 ? get_word_by_letter(median / 26) : "";
+  return front + get_word_by_letter(median % 26);
 }
 
 module.exports = median_to_letter;
