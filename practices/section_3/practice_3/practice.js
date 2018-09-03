eaad53bc7e6a09ae40cec282ad7e8bba42176c56
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 21:25:13 +0800

    update collection calculate count and minus count divided by 3

--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,5 +1,17 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let countMap = new Map();
+  collection_a.forEach(num => {
+    const default_count = (countMap.has(num)) ? countMap.get(num) : 0;
+    countMap.set(num, default_count + 1);
+  })
+  let result = [];
+  countMap.forEach((val, key) => {
+    result.push({
+      'key': key,
+      "count" :object_b.value.includes(key) ? val - parseInt(val / 3) : val
+    })
+  });
+  return result;
 }
 
 module.exports = create_updated_collection;
