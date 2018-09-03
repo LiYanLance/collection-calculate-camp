5f0acbc1626a31f6814e65ac4049a96f2810d592
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 23:36:01 +0800

    reduce compute median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  let arr = collection.concat();
+  arr.sort((a, b) => a - b);
+  const median = arr[Math.floor(arr.length / 2)];
+  return arr.length % 2 === 0 ? (median + arr[Math.floor(arr.length / 2) - 1]) / 2 : median;
 }
 
 module.exports = compute_median;
