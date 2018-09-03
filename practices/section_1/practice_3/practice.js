3dc068d32b2b122c9b52ed56f401fbb59170621a
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 19:50:38 +0800

    collect same elements in object

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,5 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  return collection_a.filter(num => object_b.value.includes(num));
 }
 
 module.exports = collect_same_elements;
