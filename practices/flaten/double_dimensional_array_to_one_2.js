ea182dac9e4cbb97a9a738b5d34c4a748c19de9d
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 17:28:28 +0800

    add flaten dimensional array to one

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let result = [];
+  collection.forEach(arr => result = result.concat(arr));
+  return Array.from(new Set(result));
 }
 
 module.exports = double_to_one;
