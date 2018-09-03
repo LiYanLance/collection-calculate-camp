6aeed4bb9d66332335a1b20e3e3f08d3b161af91
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 00:20:01 +0800

    even group calculate average

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,5 +1,31 @@
 'use strict';
-var even_group_calculate_average = function(collection){
 
+const getAverage = arr => {
+  if(arr.length === 0) return 0;
+  return arr.reduce((a, b) => a + b, 0) / arr.length
 };
+
+var even_group_calculate_average = function(collection){
+  let evens = [];
+  collection.forEach((val, index) => {
+    if(index % 2 !== 0){
+      evens.push(val);
+    }
+  })
+  evens = evens.filter(i => i % 2 == 0);
+  if(evens.length === 0){
+    return [0];
+  }
+  evens.sort((a, b) => a - b);
+  let max_length = ("" + evens[evens.length - 1]).length, result = [];
+  for(let len = 1; len <= max_length; len++){
+    let average = 0;
+    average = getAverage(evens.filter(even => ("" + even).length === len));
+    if(average !== 0){
+      result.push(average);
+    }
+  }
+  return result;
+};
+
 module.exports = even_group_calculate_average;
