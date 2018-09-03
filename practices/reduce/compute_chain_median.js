2a623d96213c4da6567690d408a4ded14b2acaf5
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 00:39:52 +0800

    compute chain median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  let arr = collection.split("->");
+  arr = arr.map(str => parseInt(str));
+  arr.sort((a, b) => a - b);
+  console.log(arr)
+  const median = arr[Math.floor(arr.length / 2)];
+  return arr.length % 2 === 0 ? (median + arr[Math.floor(arr.length / 2 - 1)]) / 2 : median;
 }
 
 module.exports = compute_chain_median;
