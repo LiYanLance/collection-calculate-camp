d1e6e523b73af9fd8d71eeff88d8f60c2acc9a94
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 17:13:49 +0800

    add filter divisible integer

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,13 @@
 'use strict';
 
-function choose_divisible_integer(collection_a, collection_b) {
+const isDivisibleByAnyElements = (a, arr) => {
+  return arr.filter(num => a % num === 0).length > 0;
+};
 
+
+function choose_divisible_integer(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.filter(num => isDivisibleByAnyElements(num, collection_b));
 }
 
 module.exports = choose_divisible_integer;
