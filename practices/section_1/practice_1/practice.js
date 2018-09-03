a268d503a3fa493c436ad2e730c492b19f984894
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 19:45:51 +0800

    collect same elements

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,5 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  return collection_a.filter(num => collection_b.includes(num));
 }
 
 module.exports = collect_same_elements;
