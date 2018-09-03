0765f67f0d98cee08e676c3f988f2808f7be95cb
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 23:03:38 +0800

    collection operator get letter interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const map_number_to_letter = number => String.fromCharCode(number + 96);
+
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let result = [...Array(Math.abs(number_a - number_b) + 1).keys()]
+    .map(x => map_number_to_letter(x + Math.min(number_b, number_a)));
+  return number_a < number_b ? result : result.reverse();
 }
 
 module.exports = get_letter_interval;
