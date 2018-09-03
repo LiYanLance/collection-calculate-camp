24d15fbe3ffd2a3e7cf2557c354826c7ffa4653f
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 23:00:35 +0800

    collection operator get integer interval with even

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const get_even = arr => arr.filter(num => num % 2 === 0);
+
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let result= [...Array(Math.abs(number_a - number_b) + 1).keys()].map(x => x + Math.min(number_b, number_a));
+  return get_even(number_a < number_b ? result : result.reverse());
 }
 
 module.exports = get_integer_interval_2;
