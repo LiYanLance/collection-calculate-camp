dce5c6d1cf34864a966ee7eb7c85aaf2d75ba63e
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 22:06:15 +0800

    subscript is even calculate average

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,11 @@
 'use strict';
 var calculate_average = function(collection){
-
+  let even_index_array = [];
+  collection.forEach((val, index) => {
+    if (index % 2 !== 0) {
+      even_index_array.push(collection[index]);
+    }
+  });
+  return even_index_array.reduce((a, b) => a + b) / even_index_array.length;
 };
 module.exports = calculate_average;
