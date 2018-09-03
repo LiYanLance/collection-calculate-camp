5bfe08fcd862aadbc7b05daeb9aade791264c1cb
LiYanLance <yanxali@CNyanxali.local>
2018-09-03 21:26:36 +0800

    update collection calculate count with dash and minus count divided by 3

--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,22 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let countMap = new Map();
+  collection_a.forEach(num => {
+    let sub_count = 1;
+    if(num.includes('-')){
+      sub_count = parseInt(num.substring(num.indexOf('-') + 1));
+      num = num.substring(0, num.indexOf('-'));
+    }
+    const default_count = (countMap.has(num)) ? countMap.get(num) : 0;
+    countMap.set(num, default_count + sub_count);
+  })
+  let result = [];
+  countMap.forEach((val, key) => {
+    result.push({
+      'key': key,
+      "count" :object_b.value.includes(key) ? val - parseInt(val / 3) : val
+    })
+  });
+  return result;
 }
 
 module.exports = create_updated_collection;
