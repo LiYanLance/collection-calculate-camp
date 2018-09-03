f0244f2df880340dce05618ddedec268d84731f9
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 01:16:33 +0800

    even single element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,11 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function (collection) {
+  let arr = [];
+  collection.forEach((val, index) => {
+    if (index % 2 !== 0) {
+      arr.push(val);
+    }
+  });
+  return Array.from(new Set(arr)).filter(i => arr.indexOf(i) == arr.lastIndexOf(i));
 };
 module.exports = single_element;
