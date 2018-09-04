284c0a9d2239aca255721336c792641651549444
LiYanLance <yanxali@CNyanxali.local>
2018-09-05 00:38:49 +0800

    refactor - use tool instead of write a method

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const getAverage = arr => arr.reduce((a, b) => a + b) / arr.length;
+const tool = require("../../../../practices/tools")
 
 var calculate_median = function(collection){
-  return getAverage(collection.filter(i => i % 2 === 0));
+  return tool.get_median(collection.filter((val, index) => index % 2 !== 0));
 };
 module.exports = calculate_median;
