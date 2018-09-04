02dfeb5dd6ea64ca0d96361569a1e95d19aaeb1a
LiYanLance <yanxali@CNyanxali.local>
2018-09-05 00:47:11 +0800

    refactor - use filter instead of forEach

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,11 +1,6 @@
 'use strict';
 var single_element = function (collection) {
-  let arr = [];
-  collection.forEach((val, index) => {
-    if (index % 2 !== 0) {
-      arr.push(val);
-    }
-  });
-  return Array.from(new Set(arr)).filter(i => arr.indexOf(i) == arr.lastIndexOf(i));
+  return collection.filter((val, index) => index % 2 !== 0)
+    .filter((val, index, self) => self.indexOf(val) == self.lastIndexOf(val));
 };
 module.exports = single_element;
