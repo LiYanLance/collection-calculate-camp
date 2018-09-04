6f95fb9fc66a8ef96d5076ce6c4dadee3062c68d
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 10:48:51 +0800

    rank by two large one small

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,19 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+
+const swap = (arr, index_a, index_b) => {
+  const temp = arr[index_a];
+  arr[index_a] = arr[index_b];
+  arr[index_b] = temp;
 }
+
+function rank_by_two_large_one_small(collection) {
+  let arr = collection.concat();
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i + 2 < arr.length - 1; i += 3) {
+    swap(arr, i, i + 1);
+    swap(arr, i + 1, i + 2);
+  }
+  return arr;
+}
+
 module.exports = rank_by_two_large_one_small;
