824ef08c516505493abd32796c4a951c97916fea
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 16:17:01 +0800

    refactor - use spread instead of forEach method

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,6 +1,5 @@
 function collect_same_elements(collection_a, collection_b) {
-  let collection = [];
-  collection_b.forEach(arr => collection = collection.concat(arr));
+  let collection = [].concat(...collection_b);
   return collection_a.filter(num => collection.includes(num));
 }
 
