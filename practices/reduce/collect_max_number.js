931650fb73ef688026b92e4017a29c265f3c4aff
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 11:27:57 +0800

    refactor - rename parameter

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function collect_max_number(collection) {
-  return collection.reduce((a, b) => a > b ? a : b);
+  return collection.reduce((max, num) => max > num ? max : num);
 }
 
 module.exports = collect_max_number;
