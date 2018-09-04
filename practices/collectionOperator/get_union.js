1521f585bf3098f4b71aff85af86ef3ef8ae2f02
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 09:49:23 +0800

    refactor - use filter instead of loop

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,14 +1,8 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
-  let collection_c = collection_a.concat();
-  collection_b.forEach(item => {
-    if (!collection_c.includes(item)) {
-      collection_c.push(item);
-    }
-  });
-  return collection_c;
+  return collection_a.concat(collection_b)
+    .filter((value, index, self) => self.indexOf(value) === index);
 }
 
 module.exports = get_union;
