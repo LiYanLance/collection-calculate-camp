3edb564b92db1c990d9afdfc925b063858b4cbca
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 12:51:03 +0800

    refactor - add default value 0 to sum

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function compute_average(collection) {
-  return collection.reduce((a, b) => a + b) / collection.length;
+  return collection.reduce((a, b) => a + b, 0) / collection.length;
 }
 
 module.exports = compute_average;
