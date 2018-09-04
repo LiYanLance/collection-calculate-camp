2f92ebc773601475f61fb505de552024a795ea7d
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 13:22:36 +0800

    refactor - sort new array not the origin collection

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,9 +1,8 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  const arr = collection.split("->")
-    .map(str => parseInt(str))
-    .sort((a, b) => a - b);
+  let arr = collection.split("->").map(str => parseInt(str));
+  arr.sort((a, b) => a - b);
   const median = arr[Math.floor(arr.length / 2)];
   return arr.length % 2 === 0 ? (median + arr[Math.floor(arr.length / 2 - 1)]) / 2 : median;
 }
--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function compute_median(collection) {
-  const arr = collection.sort((a, b) => a - b);
+  let arr = collection.concat();
+  arr.sort((a, b) => a - b);
   const median = arr[Math.floor(arr.length / 2)];
   return arr.length % 2 === 0 ? (median + arr[Math.floor(arr.length / 2) - 1]) / 2 : median;
 }
