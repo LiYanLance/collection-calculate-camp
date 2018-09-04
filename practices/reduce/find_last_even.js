99151ab46427ebdd1a63243f1d0bfe6d96808317
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 13:24:32 +0800

    refactor - find last even without changing origin colletcion

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function find_last_even(collection) {
-  return collection.filter(i => i % 2 === 0).pop();
+  const arr =  collection.filter(i => i % 2 === 0);
+  return arr[arr.length - 1];
 }
 
 module.exports = find_last_even;
