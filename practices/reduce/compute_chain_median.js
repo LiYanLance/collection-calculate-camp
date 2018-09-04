543814c6f8bf1cf94d7f289ad6f37b75b614621b
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 13:07:54 +0800

    refactor - change way of invoking method and change let to const

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,10 +1,9 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  let arr = collection.split("->");
-  arr = arr.map(str => parseInt(str));
-  arr.sort((a, b) => a - b);
-  console.log(arr)
+  const arr = collection.split("->")
+    .map(str => parseInt(str))
+    .sort((a, b) => a - b);
   const median = arr[Math.floor(arr.length / 2)];
   return arr.length % 2 === 0 ? (median + arr[Math.floor(arr.length / 2 - 1)]) / 2 : median;
 }
