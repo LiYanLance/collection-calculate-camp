19b9388d57f99933cc13e407822566add4ce0930
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 13:09:19 +0800

    refactor - change let to const

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function compute_median(collection) {
-  let arr = collection.concat();
-  arr.sort((a, b) => a - b);
+  const arr = collection.sort((a, b) => a - b);
   const median = arr[Math.floor(arr.length / 2)];
   return arr.length % 2 === 0 ? (median + arr[Math.floor(arr.length / 2) - 1]) / 2 : median;
 }
