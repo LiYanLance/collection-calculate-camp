bf4613750e2b148e16ab7510552a264f7b9e8b11
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 11:22:54 +0800

    refactor - extract method map_number_to_letter

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,9 +1,12 @@
 'use strict';
+
+const map_number_to_letter = number => {
+  const times = number > 26 ? String.fromCharCode((number - 1) / 26 + 96) : "";
+  return times + String.fromCharCode((number - 1) % 26 + 97);
+};
+
 var number_map_to_word_over_26 = function(collection){
-  return collection.map(i => {
-    let resultNum = (i > 26 ? String.fromCharCode(i / 26 + 96) : "");
-    return resultNum + String.fromCharCode(i % 26 + 96);
-  });
+  return collection.map(i => map_number_to_letter(i));
 };
 
 module.exports = number_map_to_word_over_26;
