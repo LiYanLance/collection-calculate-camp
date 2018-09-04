8beb86c52c0872ef632e7e82049987e4d40c1ad1
LiYanLance <yanxali@CNyanxali.local>
2018-09-04 16:57:08 +0800

    refactor - replace round with ceil method

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function average_to_letter(collection) {
-  return String.fromCharCode(Math.round(collection.reduce((a, b) => a + b) / collection.length) + 96);
+  return String.fromCharCode(Math.ceil(collection.reduce((a, b) => a + b) / collection.length) + 96);
 }
 
 module.exports = average_to_letter;
