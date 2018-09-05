6e1ca63c49b5cedcd1616f1f50e6aa195559a00c
LiYanLance <yanxali@CNyanxali.local>
2018-09-05 13:34:25 +0800

    refactor use map instead of forEach

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  const result= [...Array(Math.abs(number_a - number_b) + 1).keys()].map(x => x + Math.min(number_b, number_a));
+  const result= [...Array(Math.abs(number_a - number_b) + 1).keys()]
+    .map(x => x + Math.min(number_b, number_a));
   return number_a < number_b ? result : result.reverse();
 }
 
+console.log(Array(10));
 module.exports = get_integer_interval;
 
--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,7 +1,6 @@
 function collect_same_elements(collection_a, object_b) {
-  let collection = [];
-  collection_a.forEach(item => collection.push(item.key))
-  return collection.filter(num => object_b.value.includes(num));
+  return collection_a.map(item => item.key)
+    .filter(num => object_b.value.includes(num));
 }
 
 module.exports = collect_same_elements;
